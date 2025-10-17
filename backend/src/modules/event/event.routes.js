b247e5df9469a3fdc405f08f5493acf165f75256
ABHISHEK <abhidk62@gmail.com>
2025-10-17 13:09:59 +0530

    event routes documented

--- backend/src/modules/event/event.routes.js
+++ backend/src/modules/event/event.routes.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const eventController = require('./event.controller');
-
+/**
+ * @route POST /api/events
+ * @desc Create a new event
+ * @access Public
+ */
 router.post('/', eventController.createEvent);
+
+/**
+ * @route GET /api/events
+ * @desc Get all events
+ * @access Public
+ */
 router.get('/', eventController.getAllEvents);
+/**
+ * @route GET /api/profile/:profileId
+ * @desc Get events by profile id
+ * @access Public
+ */
 router.get('/profile/:profileId', eventController.getEventsByProfile);
+/**
+ * @route PUT /api/events/:id
+ * @desc Update event by event id
+ * @access Public
+ */
 router.put('/:id', eventController.updateEvent);
 
 module.exports = router;
